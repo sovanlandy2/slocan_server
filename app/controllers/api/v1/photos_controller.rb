74999755b1ee7d90d4ec17bb41522494911f760a
sovanlandy <sovanlandy@gmail.com>
2014-11-30 02:57:01 +0800

    no need page params

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PhotosController < Api::ApiController
   def index
     raise_errors_if_empty_params :user_id 
     voted_ids = User.find(params[:user_id]).votes.pluck(:photo_id)
-    photos = Photo.where("id NOT in (?)", voted_ids).order("rating desc").limit(20).shuffle
+    photos = Photo.where("id NOT in (?)", voted_ids).limit(20).shuffle
     render json: photos
   end
 end
