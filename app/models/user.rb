9c6c215e2e08f75639ac05eb061a44b24e4155df
sovanlandy <sovanlandy@gmail.com>
2014-11-30 07:12:24 +0800

    fixed deadlock

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   def top_venues
     photo_ids = self.votes.pluck(:photo_id)
     venue_ids = Photo.where(id: photo_ids).pluck(:venue_id)
+    venue_ids = [1,2,3] if venue_ids.blank?
     while venue_ids.count < 30
       venue_ids += Venue.where("id NOT in (?)",venue_ids).order("rating desc").limit(5).pluck(:id)
     end
