122f7306e5216699c81ef29f3cccc2a0b45c492f
sovanlandy <sovanlandy@gmail.com>
2014-11-30 07:29:36 +0800

    optimize n+1 query

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PhotosController < Api::ApiController
   def index
     raise_errors_if_empty_params :user_id 
     voted_ids = User.find(params[:user_id]).votes.pluck(:photo_id)
-    photos = Photo.where("id NOT in (?)", voted_ids).limit(20).shuffle
+    photos = Photo.where("photos.id NOT in (?)", voted_ids).includes(:venue).limit(20).shuffle
     render json: photos
   end
 end
