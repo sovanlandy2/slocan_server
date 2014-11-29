c84d203f8a7dc0985c4be5ffefbcb9910746b144
sovanlandy <sovanlandy@gmail.com>
2014-11-30 03:17:44 +0800

    increase number of venues that user should have if not voted

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   def top_venues
     photo_ids = self.votes.pluck(:photo_id)
     venue_ids = Photo.where(id: photo_ids).pluck(:venue_id)
-    while venue_ids.count < 10
+    while venue_ids.count < 30
       venue_ids += Venue.where("id NOT in (?)",venue_ids).order("rating desc").limit(5).pluck(:id)
     end
     venues = Venue.where(id: venue_ids).order("rating desc")
