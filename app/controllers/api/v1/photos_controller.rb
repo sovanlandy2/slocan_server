1489c283ba7915af819ea1fa0a06f060e44d3ff9
sovanlandy <sovanlandy@gmail.com>
2014-11-30 09:29:33 +0800

    fixed empty photos

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::PhotosController < Api::ApiController
   def index
     raise_errors_if_empty_params :user_id 
     voted_ids = User.find(params[:user_id]).votes.pluck(:photo_id)
-    photos = Photo.where("photos.id NOT in (?)", voted_ids).includes(:venue).limit(20).shuffle
+    if voted_ids.present?
+      photos = Photo.where("photos.id NOT in (?)", voted_ids).includes(:venue).limit(20).shuffle
+    else
+      photos = Photo.includes(:venue).limit(20).shuffle
+    end
     render json: photos
   end
 
